0ab0916658eaadff5458ca05a529e72899c1e15e
Tai <taiwoodley@gmail.com>
2024-03-12 21:47:22 +1100

    Allow server to respond to http requests

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
+import socket
 from socket import *
+import sys
 
 
 def main():
     server_socket = socket(AF_INET, SOCK_STREAM)
-    #Prepare a sever socket
-    #Fill in start
-    #Fill in end
+    # Prepare a sever socket
+    # Fill in start
+    server_socket.bind(("", 6789))
+    server_socket.listen(1)
+    # Fill in end
     while True:
-        #Establish the connection
+        # Establish the connection
         print('Ready to serve...')
-        connectionSocket, addr = # Fill in start #Fill in end
+        connection_socket, addr = server_socket.accept()
         try:
-            message = # Fill in start #Fill in end
+            message = connection_socket.recv(2048).decode()
             filename = message.split()[1]
             f = open(filename[1:])
-            outputdata = # Fill in start #Fill in end
+            output_data = f.read()
             # Send one HTTP header line into socket
             # Fill in start
+            connection_socket.send("HTTP/1.1 200 OK".encode())
             # Fill in end
             # Send the content of the requested file to the client
-            for i in range(0, len(outputdata)):
-                connectionSocket.send(outputdata[i].encode())
-            connectionSocket.send("\r\n".encode())
-            connectionSocket.close()
+            for i in range(0, len(output_data)):
+                connection_socket.send(output_data[i].encode())
+            connection_socket.send("\r\n".encode())
         except IOError:
             # Send response message for file not found
             # Fill in start
+            connection_socket.send("HTTP/1.1 404 Not Found".encode())
             # Fill in end
+        finally:
             # Close client socket
-            # Fill in start
-            # Fill in end
+            connection_socket.close()
         server_socket.close()
-        sys.exit() # Terminate the program after sending the corresponding data
+        sys.exit()  # Terminate the program after sending the corresponding data
+
 
 if __name__ == "__main__":
     main()
