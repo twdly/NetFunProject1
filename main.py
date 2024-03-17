3602a1a6cac9b04c1c574fbc5a4515ac2b70056e
AnabelSchiavone1 <161534069+AnabelSchiavone1@users.noreply.github.com>
2024-03-18 09:07:30 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,43 @@
 import socket
-from socket import *
-import sys
+from socket import * # imports all objects from the socket module
+import sys 
 
 
-def main():
-    server_socket = socket(AF_INET, SOCK_STREAM)
-    # Prepare a sever socket
-    # Fill in start
-    server_socket.bind(("", 6789))
-    server_socket.listen(1)
-    # Fill in end
+def main(): #main function to call on upon execution
+    server_socket = socket(AF_INET, SOCK_STREAM)#create new server socket
+    server_socket.bind(("", 6789)) #binds sever socket to all available network intrefaces " " with port number 6789
+    server_socket.listen(1) #allow for only 1 incoming connection
+
     while True:
-        # Establish the connection
         print('Ready to serve...')
-        connection_socket, addr = server_socket.accept()
+        connection_socket, addr = server_socket.accept()#creates a unique socket dedictaed to the specific client
+
         try:
-            message = connection_socket.recv(2048).decode()
-            filename = message.split()[1]
+            message = connection_socket.recv(2048).decode()#receives data through a http request & decodes it to a string
+            filename = message.split()[1]#splits message into parts and extracts second part which contains filename/path
             f = open(filename[1:])
             output_data = f.read()
-            # Send one HTTP header line into socket
-            # Fill in start
-            connection_socket.send("HTTP/1.1 200 OK\r\n".encode())
-            # Fill in end
-            # Send the content of the requested file to the client
-            for i in range(0, len(output_data)):
+            connection_socket.send("HTTP/1.1 200 OK\r\n".encode())#sends http response indicating a 200 ok message
+            
+            for i in range(0, len(output_data)): #iterates over every character in the string 
                 connection_socket.send(output_data[i].encode())
-            connection_socket.send("\r\n".encode())
-        except IOError:
-            # Send response message for file not found
-            # Fill in start
-            connection_socket.send("HTTP/1.1 404 Not Found\r\n".encode())
+            connection_socket.send("\r\n".encode())#indicated end of message
+
+        except IOError: #initiates an exception handler
+            connection_socket.send("HTTP/1.1 404 Not Found\r\n".encode())#sends http response indicating a 404 Not found message
             f = open("404.html")
             error_page = f.read()
+            
             for i in range(0, len(error_page)):
                 connection_socket.send(error_page[i].encode())
             connection_socket.send("\r\n".encode())
-            # Fill in end
+
         finally:
-            # Close client socket
-            connection_socket.close()
-        server_socket.close()
+            connection_socket.close() #close connection socket
+        server_socket.close()#close server socket
         sys.exit()  # Terminate the program after sending the corresponding data
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #ensure the script is executed directly 
     main()
+
